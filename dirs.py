17c8a1eba953e0cb42b00067566b9435629c72d2
Anton <116961964+Anton-Chernyshov@users.noreply.github.com>
2024-02-21 10:51:43 +0000

    Create dirs.py

---
+++ dirs.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import smIDEerrors
+def getDir() -> str|None:
+ try:
+  curDir = os.getcwd()
+ except:
+  raise smIDEerrors.DirectoryExistsError("Cannot get current directory")
+ if len(curDir) == 0:
+  return None
+ return curDir
+
+def joinDirs(*dirs:str) -> str|None:
+ output = str()
+ dirs = list(dirs)
+ if len(dirs) == 0:
+  return None
+ 
+ for dir in dirs:
+  if type(dir) == str:
+   output += dir +"\\" # dir
+ #print(output)
+
+ if "." in dirs[-1]:
+  output = output[:-1]
+ return output
+
+  
+
+
+
