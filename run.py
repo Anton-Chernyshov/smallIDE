5e3e53fde5990726c6b5cc1e758723830823214a
Anton <116961964+Anton-Chernyshov@users.noreply.github.com>
2024-03-02 12:15:19 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,3 @@
-import subprocess
-import os
-import dirs
-import smIDEerrors
-
-def main() -> None:
- try:
-    subprocess.run(["py", "smIDEmain.py"], shell=True)
- except:
-    try:
-       subprocess.run(["python" , "smIDEmain.py"], shell=True)
-    except:
-       raise smIDEerrors.PythonCommandError (f"Is python installed Globablly?? The File {__file__ } cannot access it")
-if __name__ == "__main__":
-    main()
+if __name__ =="__main__":
+    import smIDEmain
 
